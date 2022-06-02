640b1969f645f3c5e9f57e24dbd52e5d3213e1d9
alexbotelhoa <alexbotelho1@hotmail.com>
2022-06-02 09:12:12 -0300

    WIP

--- prisma/migrations/20220531193153_created_feedbacks/migration.sql
+++ prisma/migrations/20220531193153_created_feedbacks/migration.sql
@@ -4,6 +4,8 @@ CREATE TABLE `feedbacks` (
     `type` VARCHAR(191) NOT NULL,
     `comment` VARCHAR(191) NOT NULL,
     `screenshot` LONGTEXT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
